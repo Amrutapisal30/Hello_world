8616953a430c1475582b6faafde7314575ffe980
Amrutapisal30 <104000771+Amrutapisal30@users.noreply.github.com>
2022-07-19 09:26:34 +0530

    first commit

---
+++ myproject/area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int len,bre,area;
+	printf("\n enter len =");
+	scanf("%d",&len);
+	pritf("\n enter bre= ");
+	scanf("%d",&bre);
+	
+	area=len*bre;
+	printf("\n area of rectangle=%d",area);
+	printf("welcome");
+}
